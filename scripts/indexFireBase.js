ead9152b20d934b133468c9c7a821963a95c4723
Vedran Bajic <bajic.sv10.2023@uns.ac.rs>
2024-04-19 00:46:49 +0200

    Krecem sa fireBase, index napravljen da ucitava organizatore

---
+++ scripts/indexFireBase.js
@@ -0,0 +1,71 @@
+let firebaseUrl = "https://wdfestivali-default-rtdb.firebaseio.com";
+
+var getButton = document.getElementById('getButton');
+window.addEventListener('load', function (e) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                carIds = [];
+                sve = JSON.parse(request.responseText);
+                velikiDiv = document.getElementsByClassName("cards");
+
+                for(let of in sve){
+                    karta = document.createElement("div");
+                    karta.classList.add("card");
+                    karta.classList.add("card-organizator");
+
+                    podelem = document.createElement("img");
+                    podelem.src = sve[of].logo;
+                    podelem.alt = "Slika organizatora";
+                    karta.appendChild(podelem);
+                    //dodao sam sliku
+                    podelem = document.createElement("h3");
+                    podelem.innerHTML = sve[of].naziv;
+                    karta.appendChild(podelem);
+                    //dodao sam naziv
+                    podelem = document.createElement("p");
+                    podelem.innerHTML = "Kontakt telefon: ";
+
+                    podelem2 = document.createElement("b");
+                    podelem2.innerHTML = sve[of].kontaktTelefon;
+                    podelem.appendChild(podelem2);
+                    //dodao sam <b> 06165887845 </b>
+                    karta.appendChild(podelem);
+                    //dodao sam paragraf
+
+                    podelem = document.createElement("p");
+                    podelem.innerHTML = "Email: ";
+
+                    podelem2 = document.createElement("b");
+                    podelem2.innerHTML = sve[of].email;
+                    podelem.appendChild(podelem2);
+                    //dodao sam <b> kontakt@email.com </b>
+                    karta.appendChild(podelem);
+                    //dodao sam paragraf
+                    podelem = document.createElement("a");
+                    //podelem.href = "organizator.html";
+                    podelem.setAttribute("data-id", of);
+                    podelem.innerHTML = "Vise o organizatoru";
+                    podelem.onclick = dodajFestivalUPutanju;
+                    karta.appendChild(podelem);
+                    //dodao sam link
+                    velikiDiv[0].appendChild(karta);
+                }
+                // Izvajanje svakog pojedinacnog automobila iteriranjem kroz atribute objekta
+                
+            } else {
+                alert('Error occurred. Cars could not be loaded.')
+            }
+        }
+    }
+
+    request.open('GET', firebaseUrl + '/organizatoriFestivala.json');
+    request.send();
+});
+function dodajFestivalUPutanju(){
+    let clickedBtn = this;
+    console.log("mene si kliknuo");
+    let id = clickedBtn.getAttribute("data-id");
+    window.location.href = "pages/organizator.html?id="+id;
+}
